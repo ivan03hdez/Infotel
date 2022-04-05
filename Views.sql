785acb217823fad7da6314ba7dac57d9f3a57b63
Daniel <99275806+dsl32@users.noreply.github.com>
2022-04-05 11:23:45 +0200

    Actualizado al mostrarReserva

--- Views.sql
+++ Views.sql
@@ -2,17 +2,18 @@
 Vista para mostrar los datos de la reserva
 Daniel Sentamans
 */
-CREATE OR REPLACE VIEW mostrarReserva 
-AS SELECT fechaInicio, fechaFin, precioTotal 
-FROM reserva;
+CREATE OR REPLACE VIEW mostrarReserva
+AS SELECT re.codigo, re.fechaInicio, re.fechaFin, 'habitacion' AS 'habitacion/servicio', 
+rhist.idHabitacion AS 'id', rhist.precio as precio, NULL AS fecha_servicio
+FROM reservaHist rhist, reserva re, habitacion hab
+WHERE rhist.CodReserva = re.codigo
+AND hab.id = rhist.idHabitacion
+UNION SELECT  re.codigo, re.fechaInicio, re.fechaFin, 'servicio' AS 'habitacion/servicio', 
+reS.idServicio, reS.precio, reS.fecha
+FROM reservaServicio reS, reserva re
+WHERE re.codigo = reS.codReserva;
 
-/*
-Vista para mostrar los datos de los servicios reservados
-Daniel Sentamans
-*/
-CREATE OR REPLACE VIEW mostrarReservaServicios 
-AS SELECT fecha, codReserva, idEmpleado, precio
-FROM reservaServicio;
+
 
 /*
 Vista para mostrar los datos de la habitación
@@ -26,7 +27,7 @@ FROM habitacion;
 Vista para obtener los codigos de reserva asociados a un cliente y su estado de pago
 A. TARI
 */
-CREATE VIEW reservas_clientes AS SELECT DISTINCT c.nif, c.nombre , c.apellidos, rh.CodReserva, IF(reservaPagada(rh.CodReserva), 'Pagada', 'Pendiente de pago') AS 'Estado de pago'
+CREATE OR REPLACE VIEW reservas_clientes AS SELECT DISTINCT c.nif, c.nombre , c.apellidos, rh.CodReserva, IF(reservaPagada(rh.CodReserva), 'Pagada', 'Pendiente de pago') AS 'Estado de pago'
 FROM reservaHist rh, cliente c where rh.idCliente = c.id;
 
 /*
@@ -37,10 +38,4 @@ CREATE OR REPLACE
 VIEW Adaptada AS SELECT id 
 FROM habitacion 
 WHERE esAdaptada = 1;
-/* Vista para ver las habitaciones libres por planta Juan Vercher*/
-
-Create view habitaciones_libre  
-as select numero, codhotel, substring(convert(numero,varchar(4)),1,1) 
-as 'Planta' from habitacion where numero < 100>199 and esta ocupada = false
-count numero
 
