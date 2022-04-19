ff491eb79acdc0ee22de6ffcb7bdd22e507bd9d8
Ivan Hernandez Illan <ihi1@mscloud.ua.es>
2022-04-19 21:11:46 +0200

    proyecto configurado para poder empezar a incrustar html y css y obtener los datos de la BBDD

---
+++ mvc/index.php
@@ -0,0 +1,29 @@
+<?php
+    define("BASE_DIR","C:/xampp/htdocs/infotel/mvc");
+    define("MOD_DIR",BASE_DIR."/modelos/");
+    define("VIEW_DIR",BASE_DIR."/vistas/");
+    #Recoge por GET el identificador de la página a mostrar.
+    if (array_key_exists("ID",$_GET)){
+        $pagina=$_GET["ID"];
+        $datos=$_POST;
+    }else{ // Clase a cargar por defecto)
+        $pagina="Home";
+        $datos=array();
+    }
+
+    // si no existe la pagina demandada le mandamos al Home
+    $modelo="${pagina}Modelo";
+    if (!file_exists($modelo))
+        $pagina="Home";
+
+    $modelo="${pagina}Modelo";
+    require (MOD_DIR."$modelo.php");
+    $m=new $modelo;
+    $resultado=$m->Ejecuta($datos);
+    #Carga la vista y la ejecuta
+    $vista="${pagina}Vista";
+    require (VIEW_DIR."$vista.php");
+    $v=new $vista;
+    $v->render($resultado);
+    //echo phpinfo()
+?>
---
+++ mvc/vistas/HomeVista.php
@@ -0,0 +1,17 @@
+<?php
+    require_once "plantillas//PlantillaHtmlVista.php";
+
+    class HomeVista extends PlantillaHtmlVista {
+        function render($datos_in){
+            $this->codigoHtml = <<<HTML
+            <H1>Datos de Salida</H1>
+            <table class="table table-bordered">
+                <tr><td>DATO</td><td>VALOR</td></tr>
+                <tr><td>datoX</td><td>4</td></tr>
+            </table>
+            HTML;
+
+            echo parent::render(NULL);
+        }
+    }
+?>
