9dccead962f23c07804cf195f425642fa31fb326
Juan <javm10@gcloud.ua.es>
2022-06-05 16:20:20 +0200

    Reserva historico

--- mvc/modelos/MisViajesModelo.php
+++ mvc/modelos/MisViajesModelo.php
@@ -1,12 +1,52 @@
 <?php
     class MisViajesModelo {
-        public function getDatos($datos_in){
-        /*Realiza todas las acciones necesarias con el array $datos_in
-        ...
-        */
-        #Recoge los resultados en un array y lo devuelve
-        $salida=["datoX"=>"ValorX", "datosY"=>"ValorY", "datosZ"=>"ValorZ"];
-        return $salida;
+       /* public function getDatos($datos_in){
+            try {
+                //Consulta a partir del id de la dirección de un usuario
+                $query = DatabaseConnection::query('select * from reserva LIMIT 10');
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                exit();
+            }
+
+        $dire = array();
+
+        while($row = mysqli_fetch_array($query)) 
+        {
+            $dire[] = $row;
         }
+        session_start();
+        $_SESSION['reserva'] = count($dire) > 0 ? $dire[0] : null;
+     
+
+        return $dire;
+   
+    }*/
+    
+       // Con esta saco el precio de las habitaciones
+    public function getDatos($datos_in){
+            try {
+                $datos_in = 1;
+                //Consulta a partir del id de la dirección de un usuario
+                $query = DatabaseConnection::query('select precio from reservaHist rh, reserva r where r.Codigo = rh.CodReserva AND rh.CodReserva = "'.$datos_in.'"');
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                exit();
+            }
+
+        $dire = array();
+
+        while($row = mysqli_fetch_array($query)) 
+        {
+            $dire[] = $row;
+        }
+        session_start();
+        $_SESSION['reserva'] = count($dire) > 0 ? $dire[0] : null;
+    
+
+        return $dire;
+
+    }
+
     }
 ?>
--- mvc/vistas/MisViajesVista.php
+++ mvc/vistas/MisViajesVista.php
@@ -6,16 +6,113 @@
         public function render($datos_in) {
             /*session_start(); 
             $user = $_SESSION['user'];*/
+            session_start();
+            if (!isSet($_SESSION['user'])) {
+                return header("Location: login");
+            }
+        
+            $user = $_SESSION['user'];
+            $reserva = $_SESSION['reserva'];
             $datos = [
                 'tituloPagina' => 'Mis Viajes'
             ];
             $mainMenu = MenuPrincipalVista::getMainMenu($datos);
+            $tipos_habitaciones = MisViajesVista::generarHTMLtiposhabitaciones($datos_in);
+            $res = MisViajesVista::getReserva($datos_in);
+            $reshabitacion =  MisViajesVista::reservaHabitacion($datos_in);
             $this->bodyPagina = <<<HTML
                 $mainMenu
+                <body>
+                <div class="main-content">
+                    <div class="container mt-7">
+                    <!-- Table -->
+                    <h2 class="mb-5">Reserva Histórico</h2>
+                    
+                             $reshabitacion
+                        
+                    </div>
+                </body>
             HTML;
             $this->tituloPagina = "Mis Viajes";
 
             echo parent::render(NULL);
         }
+        public function reservaHabitacion($datos_in){
+            $codHTML = "";
+            foreach($datos_in as $reserva){
+                    $pos = 1 ;
+                $codHTML .= <<< HTML
+                  <div class="row">
+                                    <div class="col">
+                                        <label class="form-control-label" for="input-DNI">Precio de la habitacion {$pos}</label>
+                                        <input type="text" id="input-DNI" class="form-control form-control-alternative" placeholder="DNI" value="{$reserva["precio"]}">
+                                    </div>               
+                 </div>
+                HTML; 
+              
+            }
+            return $codHTML;
+        }
+        public function generarHTMLtiposhabitaciones($datos_in){
+            $codHTML = "";
+            foreach($datos_in as $reserva){
+                $codHTML .= <<< HTML
+                  <div class="row">
+                                    <div class="col">
+                                        <label class="form-control-label" for="input-DNI">Código reserva</label>
+                                        <input type="text" id="input-DNI" class="form-control form-control-alternative" placeholder="DNI" value="{$reserva["Identificador"]}">
+                                    </div>
+                                    <div class="col">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="input-last-name">Fecha Inicio</label>
+                                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="{$reserva["fechaInicio"]}">
+                                    </div>
+                                    </div>
+                                    <div class="col">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="input-last-name">Fecha Fin</label>
+                                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="{$reserva["fechaFin"]}">
+                                    </div>
+                                    </div>
+                                    <div class="col">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="input-last-name">PrecioTotal</label>
+                                        <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="{$reserva["precioTotal"]}">
+                                    </div>
+                                    </div>
+                                   <div class="btn btn-primary" onclick="getReserva($reserva)">Mas     
+                                    </div>
+                                   
+                 </div>
+
+                HTML; 
+            }
+            return $codHTML;
+        }
+                public function getReserva($datos_in){
+                
+                    $query = DatabaseConnection::query('select rh.id from reservaHist rh, reserva r where r.Codigo = rh.CodReserva AND rh.CodReserva ="'.$datos_in.'"');
+                    $codHTML = "";
+                    while($row = mysqli_fetch_array($query)) 
+                            {
+                                $hotels[] = $row;
+                            }
+                                    
+                        $codHTML .= <<< HTML
+                         <div class="row">
+                            foreach($$hotels as $reserva){
+                                
+                                <div class="form-group focused">
+                                        <label class="form-control-label" for="input-last-name">Fecha Inicio</label>
+                                        <input type="text" id="input-last-name" class="form-control form-control-alternative" value="{$reserva["fechaInicio"]}">
+                                    </div>
+                                    </div>
+
+                            endforeach
+                            </div>
+                        HTML; 
+                    
+                    return $codHTML;
+            }
     }
 ?>
